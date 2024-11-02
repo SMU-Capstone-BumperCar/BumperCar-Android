86ae2edc27bfff7be7d05cc490c0a3df901224fb
JANG JI YONG <zdy2010@naver.com>
2024-11-03 05:17:36 +0900

    feat: 과실비율 답변 호출 ApiService 추가

--- app/src/main/java/com/example/bumpercar/network/ChatApiService.kt
+++ app/src/main/java/com/example/bumpercar/network/ChatApiService.kt
@@ -1,6 +1,14 @@
 package com.example.bumpercar.network
 
+import com.example.bumpercar.data.Message
+import com.example.bumpercar.data.JudgeResponseData
+import retrofit2.Response
+import retrofit2.http.Body
+import retrofit2.http.POST
+
 interface ChatApiService {
-
-
+    @POST("drive-judge")
+    suspend fun postDriveJudge(
+        @Body query: Message
+    ) : Response<JudgeResponseData>
 }
