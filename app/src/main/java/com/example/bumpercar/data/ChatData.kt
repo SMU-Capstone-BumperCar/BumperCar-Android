083f2c9d1a57ae59030b050b9b6dcecb98b7d6b1
JANG JI YONG <zdy2010@naver.com>
2024-11-03 02:17:43 +0900

    feat: ChatData 클래스 생성

--- app/src/main/java/com/example/bumpercar/data/ChatData.kt
+++ app/src/main/java/com/example/bumpercar/data/ChatData.kt
@@ -1,16 +1,15 @@
 package com.example.bumpercar.data
 
 data class ChatData(
-    val message: List<Message>,
-    val address: Author
+    val messages: List<Message>,
+    val interlocutor: Author
 ) {
     data class Message(
         val text: String,
         val author: Author,
-        val icon: Int? = null
     ) {
         val isFromMe: Boolean
-            get() = author.id == MY_ID
+            get() = author.id == Author.MY_ID
     }
 
     data class Author(
@@ -18,12 +17,9 @@ data class ChatData(
         val name: String
     ) {
         companion object {
-            val bot = Author("1", "bot")
-            val me = Author(MY_ID, "ME")
+            const val MY_ID = "-1"
+            val chatBotAssistant = Author("1", "ChatBotAssistant")
+            val localUser = Author(MY_ID, "ME")
         }
-    }
-
-    companion object {
-        const val MY_ID = "-1"
     }
 }
