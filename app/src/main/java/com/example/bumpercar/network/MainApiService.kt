bec5c73ec24a2d0acd88cad80b8b31e32381636b
JANG JI YONG <zdy2010@naver.com>
2024-11-04 04:13:17 +0900

    chore: Test로 사용했던 데이터 클래스 변경

--- app/src/main/java/com/example/bumpercar/network/MainApiService.kt
+++ app/src/main/java/com/example/bumpercar/network/MainApiService.kt
@@ -2,6 +2,7 @@ package com.example.bumpercar.network
 
 import com.example.bumpercar.data.MessageData
 import com.example.bumpercar.data.JudgeResponseData
+import com.example.bumpercar.data.ReviewData
 import retrofit2.Response
 import retrofit2.http.Body
 import retrofit2.http.POST
@@ -14,6 +15,6 @@ interface MainApiService {
 
     @POST("summarize_review")
     suspend fun postReview(
-        @Body query: MessageData
-    ) : Response<JudgeResponseData>
+        @Body body: Map<String, String> // JSON 객체 형태로 변경
+    ): Response<ReviewData>
 }
