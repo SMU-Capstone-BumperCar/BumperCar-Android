8f4dbe33ce5d3f590efbd0214072731c932901ef
JANG JI YONG <zdy2010@naver.com>
2024-11-04 04:17:57 +0900

    feat: ReviewMainScreen 및 ReviewScreen 추가

--- app/src/main/java/com/example/bumpercar/navigation/RootNavGraph.kt
+++ app/src/main/java/com/example/bumpercar/navigation/RootNavGraph.kt
@@ -1,33 +1,57 @@
 package com.example.bumpercar.navigation
 
 import androidx.compose.runtime.Composable
+import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavHostController
+import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
-import com.example.bumpercar.screen.ChatInfoScreen
+import androidx.navigation.navArgument
+import com.example.bumpercar.screen.ChatMainScreen
 import com.example.bumpercar.screen.ChatScreen
 import com.example.bumpercar.screen.MainScreen
+import com.example.bumpercar.screen.ReviewMainScreen
 import com.example.bumpercar.screen.ReviewScreen
-import com.example.bumpercar.viewmodel.ChatViewModel
+import com.example.bumpercar.viewmodel.MainViewModel
 
 @Composable
 fun RootNavGraph(
     navHostController: NavHostController
 ) {
-    val chatViewModel = ChatViewModel()
+    val mainViewModel: MainViewModel = viewModel()
 
     NavHost(navController = navHostController, startDestination = Route.HOME) {
         composable(Route.HOME) {
             MainScreen()
         }
         composable(Route.CHAT_INFO) {
-            ChatInfoScreen(navHostController = navHostController)
+            ChatMainScreen(navHostController = navHostController)
         }
         composable(Route.REVIEW) {
-            ReviewScreen()
+            ReviewMainScreen(navHostController = navHostController, viewModel = mainViewModel)
+        }
+        composable(
+            "reviewScreen/{hospitalName}/{hospitalPhone}/{hospitalLocation}",
+            arguments = listOf(
+                navArgument("hospitalName") { type = NavType.StringType },
+                navArgument("hospitalPhone") { type = NavType.StringType },
+                navArgument("hospitalLocation") { type = NavType.StringType }
+            )
+        ) { backStackEntry ->
+            val hospitalName = backStackEntry.arguments?.getString("hospitalName")
+            val hospitalPhone = backStackEntry.arguments?.getString("hospitalPhone")
+            val hospitalLocation = backStackEntry.arguments?.getString("hospitalLocation")
+
+            ReviewScreen(
+                hospitalName = hospitalName ?: "",
+                hospitalPhone = hospitalPhone ?: "",
+                hospitalLocation = hospitalLocation ?: "",
+                navHostController = navHostController,
+                viewModel = mainViewModel
+            )
         }
         composable(Route.CHAT) {
-            ChatScreen(chatViewModel, navHostController)
+            ChatScreen(mainViewModel, navHostController)
         }
     }
 }
