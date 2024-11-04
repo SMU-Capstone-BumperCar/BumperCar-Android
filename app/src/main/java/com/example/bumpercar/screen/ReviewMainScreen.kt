44106a8f68a0a33d6ba0efc56292bdd552827f99
JANG JI YONG <zdy2010@naver.com>
2024-11-05 06:09:43 +0900

    feat : 아이콘, 리뷰 수 ui 추가

--- app/src/main/java/com/example/bumpercar/screen/ReviewMainScreen.kt
+++ app/src/main/java/com/example/bumpercar/screen/ReviewMainScreen.kt
@@ -1,22 +1,35 @@
 package com.example.bumpercar.screen
 
+import android.content.Context
+import androidx.annotation.DrawableRes
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.buildAnnotatedString
 import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
@@ -31,6 +44,8 @@ import com.example.bumpercar.hospital.hospitalList
 import com.example.bumpercar.ui.theme.mainHeadLineColor
 import com.example.bumpercar.ui.theme.mainSubLineColor
 import com.example.bumpercar.viewmodel.MainViewModel
+import com.google.android.gms.maps.model.LatLng
+import com.google.maps.android.compose.Marker
 import java.net.URLEncoder
 
 @Composable
@@ -53,7 +68,7 @@ fun ReviewMainScreen(
 
         Column(
             modifier = Modifier.padding(horizontal = 16.dp)
-        ){
+        ) {
             Text(
                 text = buildAnnotatedString {
                     withStyle(
@@ -78,11 +93,6 @@ fun ReviewMainScreen(
             )
 
             Spacer(modifier = Modifier.height(16.dp))
-            
-            Text(
-                text = "해당 병원들은 리뷰 수가 많은 순으로 정렬되어 있습니다",
-
-            )
 
             if (isLoading.value) {
                 // 로딩 중일 때 보여줄 UI
@@ -95,6 +105,17 @@ fun ReviewMainScreen(
                     CircularProgressIndicator() // 로딩 애니메이션
                 }
             } else {
+                Text(
+                    text = "해당 병원들은 리뷰 수가 많은 순으로 정렬되어 있습니다",
+                    style = TextStyle(
+                        fontFamily = FontFamily(Font(R.font.notosans_bold)),
+                        fontSize = 14.sp,
+                        color = Color.Black
+                    )
+
+                )
+
+                Spacer(modifier = Modifier.height(10.dp))
                 // 로딩 중이 아닐 때 LazyColumn 보여주기
                 LazyColumn {
                     items(hospitalList.size) { index ->
@@ -103,17 +124,61 @@ fun ReviewMainScreen(
                             modifier = Modifier.clickable {
                                 mainViewModel.getReviewResponse(hospital.hospitalName) {
                                     navHostController.navigate(
-                                        "reviewScreen/${URLEncoder.encode(hospital.hospitalName, "UTF-8").replace("+", "%20")}/${URLEncoder.encode(hospital.hospitalPhone, "UTF-8")}/${URLEncoder.encode(hospital.hospitalAddress, "UTF-8").replace("+", "%20")}"
+                                        "reviewScreen/${
+                                            URLEncoder.encode(
+                                                hospital.hospitalName,
+                                                "UTF-8"
+                                            ).replace("+", "%20")
+                                        }/${
+                                            URLEncoder.encode(
+                                                hospital.hospitalPhone,
+                                                "UTF-8"
+                                            )
+                                        }/${
+                                            URLEncoder.encode(hospital.hospitalAddress, "UTF-8")
+                                                .replace("+", "%20")
+                                        }"
                                     )
                                 }
                             }
                         ) {
-                            Text(
-                                text = hospital.hospitalName,
-                                fontSize = 20.sp,
-                                fontFamily = FontFamily(Font(R.font.notosans_bold)),
-                                color = mainHeadLineColor
-                            )
+                            Row(
+                                modifier = Modifier
+                                    .fillMaxWidth()
+                                    .padding(vertical = 10.dp),
+                                horizontalArrangement = Arrangement.spacedBy(5.dp),
+                                verticalAlignment = Alignment.CenterVertically
+                            ) {
+                                Image(
+                                    modifier = Modifier
+                                        .size(20.dp)
+                                        .padding(3.dp),
+                                    painter = painterResource(id = R.drawable.ic_hospital),
+                                    contentDescription = "병원"
+                                )
+                                Text(
+                                    modifier = Modifier,
+                                    text = hospital.hospitalName,
+                                    style = TextStyle(
+                                        fontFamily = FontFamily(Font(R.font.notosans_bold)),
+                                        fontSize = 20.sp,
+                                        color = mainHeadLineColor
+                                    )
+                                )
+                                Box(
+                                    modifier = Modifier.weight(1f),
+                                    contentAlignment = Alignment.CenterEnd
+                                ) {
+                                    Text(
+                                        text = "${hospital.reviewCount}개의 리뷰",
+                                        style = TextStyle(
+                                            fontFamily = FontFamily(Font(R.font.notosans_bold)),
+                                            fontSize = 12.sp,
+                                            color = mainHeadLineColor
+                                        )
+                                    )
+                                }
+                            }
                         }
                         Spacer(modifier = Modifier.height(16.dp))
                     }
