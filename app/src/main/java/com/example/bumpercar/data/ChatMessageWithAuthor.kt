b48ade7ad9e08edb3c57218fd37fba3c98ded25d
JANG JI YONG <zdy2010@naver.com>
2024-11-05 06:07:58 +0900

    feat : 애니메이션 관리 여부 추가

--- app/src/main/java/com/example/bumpercar/data/ChatMessageWithAuthor.kt
+++ app/src/main/java/com/example/bumpercar/data/ChatMessageWithAuthor.kt
@@ -2,5 +2,6 @@ package com.example.bumpercar.data
 
 data class ChatMessageWithAuthor (
     val messageData: MessageData,
-    val authorData: AuthorData
+    val authorData: AuthorData,
+    var hasAnimated: Boolean = false // 애니메이션 완료 여부
 )
