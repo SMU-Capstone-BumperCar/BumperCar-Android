69c4c6a21860d6e35ff1baaa27e0347e1cd2abf6
JANG JI YONG <zdy2010@naver.com>
2024-11-05 06:05:35 +0900

    chore : ROUT 이름 변경

--- app/src/main/java/com/example/bumpercar/navigation/Route.kt
+++ app/src/main/java/com/example/bumpercar/navigation/Route.kt
@@ -2,7 +2,8 @@ package com.example.bumpercar.navigation
 
 object Route {
     const val HOME = "home"
-    const val CHAT_INFO = "chatInfo"
-    const val REVIEW = "review"
+    const val CHAT_MAIN = "chatMain"
+    const val REVIEW_MAIN = "reviewMain"
     const val CHAT = "chat"
+    const val REVIEW = "reviewScreen/{hospitalName}/{hospitalPhone}/{hospitalLocation}"
 }
